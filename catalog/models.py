8cb8fc7eafc364fcc5ae19be451f4989283c083c
Yurii <yurii.1986.e@gmail.com>
2025-06-19 18:26:12 +0400

    На проверку.

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+    """ Модель Category"""
+
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.TextField(max_length=500, verbose_name='описание')
+
+    def __str__(self):
+        """ Вывод информации"""
+
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    """ Модель Product"""
+
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.TextField(max_length=500, verbose_name='описание')
+    image = models.ImageField(upload_to='images/', null=True, verbose_name='изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    price = models.DecimalField(decimal_places=10, max_digits=14, null=True, verbose_name='цена')
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        """ Вывод информации"""
+
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,12 @@
-""" Конфигурация URL для проекта config.
-
-Список urlpatterns сопоставляет URL-адреса с представлениями.
-Дополнительную информацию см.: https://docs.djangoproject.com/en/5.2/topics/http/urls
-Примеры: Представления-функции
-1. Добавьте импорт: от моего_приложения импортируйте представления
-2. Добавьте URL в список urlpatterns: path('', views.home, name='home')
-Представления на основе классов
-1. Добавьте импорт: от представлений другого_приложения импортируйте Home
-2. Добавьте URL в список urlpatterns: path('', Home.as_view(), name='home')
-Подключение другой конфигурации URL
-1. Импортируйте функцию include(): от django.urls импортируйте include, path
-2. Добавьте URL в список urlpatterns: path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
