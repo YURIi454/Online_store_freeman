5db7c7900f92b512adc939fac1bebbdf5f62da04
Yurii <yurii.1986.e@gmail.com>
2025-06-25 03:06:49 +0400

    На проверку.

--- catalog/models.py
+++ catalog/models.py
@@ -11,7 +11,7 @@ class Category(models.Model):
     def __str__(self):
         """ Вывод информации"""
 
-        return self.name
+        return f'{self.name} {self.description}'
 
     class Meta:
         verbose_name = 'Категория'
@@ -22,17 +22,17 @@ class Product(models.Model):
     """ Модель Product"""
 
     name = models.CharField(max_length=150, verbose_name='наименование')
-    description = models.TextField(max_length=500, verbose_name='описание')
+    description = models.TextField(max_length=1200, verbose_name='описание')
     image = models.ImageField(upload_to='images/', null=True, verbose_name='изображение')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    price = models.DecimalField(decimal_places=10, max_digits=14, null=True, verbose_name='цена')
+    price = models.DecimalField(decimal_places=2, max_digits=14, null=True, verbose_name='цена')
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
         """ Вывод информации"""
 
-        return self.name
+        return f'{self.name} {self.description} {self.price}'
 
     class Meta:
         verbose_name = 'Продукт'
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from catalog.views import home, contacts
+from . import views
+
+app_name = 'catalog'
 
 urlpatterns = [
-    path("", home),
-    path("home/", home),
-    path("contacts/", contacts),
+    path("", views.catalog_all_product, name="all_products"),
+    path("contacts/", views.catalog_contacts, name="contacts"),
+    path("one_product/<int:product_id>/", views.catalog_one_product, name="one_product"),
+    path("pay/", views.catalog_pay, name="pay"),
 ]
